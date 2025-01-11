45529b8123eb8db8c41af31ac450b0372fce1d5c
Julia <julialopezcalvo95@gmail.com>
2025-01-11 10:12:19 +0100

    variables

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,9 @@
 console.log('>> Ready :)');
 
 //SECCION COLORES
+
+//Variables
+const green = document.querySelector('.js_green');
+const pink = document.querySelector('.js_pink');
+const purple = document.querySelector('.js_purple');
+const orange = document.querySelector('.js_orange ');
