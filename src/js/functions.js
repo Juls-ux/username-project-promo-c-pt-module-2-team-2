27714a1c27f8ca7309425dcfa5816fc49514c6fb
estherbg85 <estherbonillagirol85@gmail.com>
2025-01-13 18:56:25 +0100

    conflict

--- src/js/functions.js
+++ src/js/functions.js
@@ -24,118 +24,82 @@ buttonDelet.addEventListener("click", (ev) => {
 
 // Functions
 
-function createProduct(){
+function createProduct() {
+  const imageUrl = document.getElementById("imageUpload").value;
+  const price = document.getElementById("number").value;
+  const name = document.getElementById("name").value;
 
-const imageUrl= document.getElementById("imageUpload").value;
-const price= document.getElementById("number").value;
-const name= document.getElementById("name").value;
-
-const product = {
+  const product = {
     id: Date.now(),
     imageUrl: imageUrl,
     price: price,
     name: name,
-};
- 
-arrayOfProducts.push(product);
-console.log(arrayOfProducts)
+  };
+
+  arrayOfProducts.push(product);
+  console.log(arrayOfProducts);
 }
 
 function renderOneProduct(objProduct) {
-    return `<li class="container">
+  return `<li class="container">
                 <span class="container_erase js_cardsEraser" data-id="${objProduct.id}">X</span>
                 <img src="${objProduct.imageUrl} class="container_img" 
-<<<<<<< HEAD
-                alt="character image ${objProduct.name}"/>
-=======
                 alt="product image ${objProduct.name}"/>
->>>>>>> 5e4c381fb0aa4f7f4efcebe16754f798ec9cf293
                 <p class="container_price">${objProduct.price}</p>
                 <p class="container_name">${objProduct.name}</p>
             </li>`;
-  };
+}
 
-  function renderAllProducts(arrayOfProducts) {
-    let html = "";
-    for(const objProduct of arrayOfProducts){
-        html+= renderOneProduct(objProduct);
-    }
-    productList.innerHTML=html;
-    attachClickEventsToEraserCards();
-<<<<<<< HEAD
-=======
-    localStorage.setItem("products", JSON.stringify(arrayOfProducts))
->>>>>>> 5e4c381fb0aa4f7f4efcebe16754f798ec9cf293
-    
-};
+function renderAllProducts(arrayOfProducts) {
+  let html = "";
+  for (const objProduct of arrayOfProducts) {
+    html += renderOneProduct(objProduct);
+  }
+  productList.innerHTML = html;
+  attachClickEventsToEraserCards();
+  localStorage.setItem("products", JSON.stringify(arrayOfProducts));
+}
 
 function attachClickEventsToEraserCards() {
-    const cardsEraser = document.querySelectorAll(".js_cardsEraser");
-    /*Añade evento click a cada <span>*/
-    for (const eraser of cardsEraser) {
-      eraser.addEventListener("click", (ev) => {
+  const cardsEraser = document.querySelectorAll(".js_cardsEraser");
+  /*Añade evento click a cada <span>*/
+  for (const eraser of cardsEraser) {
+    eraser.addEventListener("click", (ev) => {
       ev.preventDefault();
-  
-  
-<<<<<<< HEAD
-        /*Coge el atributo nuevo data-id como ancla para coger el objeto del array favorites y eliminarlo del array favorites */
-        const idProduct = ev.currentTarget.getAttribute("data-id");
-        const index = arrayOfProducts.findIndex((prod) => prod.id === parseInt(idProduct));
-  
-        /*Si el índice es distinto de -1, significa que el objeto se encontró en el array favorites y lo eliminamos */
-        if (index !== -1) {
-            arrayOfProducts.splice(index, 1);
-            localStorage.setItem("products", JSON.stringify(arrayOfProducts));
-            renderAllProducts();
-=======
-        /*Coge el atributo nuevo data-id como ancla para coger el objeto del arrayOfProducts y eliminarlo del arrayOfProducts */
-        const idProduct = ev.currentTarget.getAttribute("data-id");
-        const index = arrayOfProducts.findIndex((prod) => prod.id === parseInt(idProduct));
-  
-        /*Si el índice es distinto de -1, significa que el objeto se encontró en el arrayOfProducts y lo eliminamos */
-        if (index !== -1) {
-            arrayOfProducts.splice(index, 1);
-            localStorage.setItem("products", JSON.stringify(arrayOfProducts));
-            renderAllProducts(arrayOfProducts);
->>>>>>> 5e4c381fb0aa4f7f4efcebe16754f798ec9cf293
-        };
-      });
-    };
-  };
 
-  function getProductsLocalstorage() {
-<<<<<<< HEAD
-    const savedpProducts = JSON.parse(localStorage.getItem("products"));
-  
-    if (Array.isArray(savedpProducts)) {
-        arrayOfProducts = savedFavorites;
-=======
-    const savedProducts = JSON.parse(localStorage.getItem("products"));
-  
-    if (Array.isArray(savedProducts)) {
-        arrayOfProducts = savedProducts;
->>>>>>> 5e4c381fb0aa4f7f4efcebe16754f798ec9cf293
-    } else {
-        arrayOfProducts = []; 
-    };
-  
-<<<<<<< HEAD
-    renderAllProducts();
-=======
-    renderAllProducts(arrayOfProducts);
->>>>>>> 5e4c381fb0aa4f7f4efcebe16754f798ec9cf293
-  };
+      /*Coge el atributo nuevo data-id como ancla para coger el objeto del arrayOfProducts y eliminarlo del arrayOfProducts */
+      const idProduct = ev.currentTarget.getAttribute("data-id");
+      const index = arrayOfProducts.findIndex(
+        (prod) => prod.id === parseInt(idProduct)
+      );
 
-  function removeProductsLocalStorage() {
-    localStorage.removeItem("products");
-<<<<<<< HEAD
+      /*Si el índice es distinto de -1, significa que el objeto se encontró en el arrayOfProducts y lo eliminamos */
+      if (index !== -1) {
+        arrayOfProducts.splice(index, 1);
+        localStorage.setItem("products", JSON.stringify(arrayOfProducts));
+        renderAllProducts(arrayOfProducts);
+      }
+    });
   }
-  
-=======
+}
+
+function getProductsLocalstorage() {
+  const savedProducts = JSON.parse(localStorage.getItem("products"));
+
+  if (Array.isArray(savedProducts)) {
+    arrayOfProducts = savedProducts;
+  } else {
     arrayOfProducts = [];
   }
+
+  renderAllProducts(arrayOfProducts);
+}
+
+function removeProductsLocalStorage() {
+  localStorage.removeItem("products");
+  arrayOfProducts = [];
+}
 
 // call functions
 
 getProductsLocalstorage();
->>>>>>> 5e4c381fb0aa4f7f4efcebe16754f798ec9cf293
